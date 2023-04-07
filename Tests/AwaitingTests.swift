3e18cac6007c84211727aa59701bd48f9dd77cd7
Simon Whitty <simon@whileloop.com>
2023-04-07 19:02:37 +1000

    indentation

--- Tests/AwaitingTests.swift
+++ Tests/AwaitingTests.swift
@@ -34,211 +34,211 @@ import XCTest
 
 final class AwaitingTests: XCTestCase {
 
-  func testWrappedValueUpdates() {
-    let mock = Mock("")
-    mock.property = "Shrimp"
-    XCTAssertEqual(mock.property, "Shrimp")
-  }
-
-  func testInitializesWithValue() async throws {
-    // given
-    let mock = Mock<Int?>(10)
-
-    // when
-    let value = try await mock.$property.some()
-
-    // then
-    XCTAssertEqual(value, 10)
-  }
-
-  func testLateTask_ReceivesTheWrappedValue() async throws  {
-    // given
-    let mock = Mock<String?>("Fish")
-    mock.property = "Chips"
-
-    // when
-    let value = try await mock.$property.some()
-
-    // then
-    XCTAssertEqual(value, "Chips")
-  }
-
-  func testMultipleTasks_ReceiveTheWrappedValue() async throws {
-    // given
-    let mock = Mock<String?>(nil)
-
-    async let value1 = mock.$property.some()
-    async let value2 = mock.$property.some()
-    async let value3 = mock.$property.some()
-
-    // when
-    Task { mock.property = "Chips" }
-
-    // then
-    let v1 = try await value1
-    XCTAssertEqual(v1, "Chips")
-
-    let v2 = try await value2
-    XCTAssertEqual(v2, "Chips")
-
-    let v3 = try await value3
-    XCTAssertEqual(v3, "Chips")
-  }
-
-  func testWaitersAreRemoved_WhenComplete() async throws  {
-    // given
-    let mock = Mock<String?>(nil)
-    async let value1 = mock.$property.some()
-
-    // when
-    mock.property = "Chips"
-    _ = try await value1
-
-    // then
-    XCTAssertTrue(mock.isWaitingEmpty)
-  }
-
-  func testNil_MakesTaskWait() async throws {
-    // given
-    let mock = Mock<String?>("Fish")
-    mock.property = "Fish"
-    mock.property = nil
-    async let value1 = mock.$property.some()
-
-    // when
-    mock.property = "Chips"
-
-    // then
-    let v1 = try await value1
-    XCTAssertEqual(v1, "Chips")
-  }
-
-  func testCancellingTask_ThrowsCancellationError() async {
-    // given
-    let mock = Mock<String?>(nil)
-    let task = Task<String, Error> {
-      try await mock.$property.some()
+    func testWrappedValueUpdates() {
+        let mock = Mock("")
+        mock.property = "Shrimp"
+        XCTAssertEqual(mock.property, "Shrimp")
     }
 
-    // when
-    task.cancel()
-
-    // then
-    do {
-      _ = try await task.value
-      XCTFail("Expected Error")
-    } catch {
-      XCTAssertTrue(error is CancellationError)
-    }
-  }
-
-  func testCollectionWaiter_WaitsForMinimumElements() async throws  {
-    let mock = Mock("")
-    async let value = mock.$property.first(withAtLeast: 5)
-
-    // when
-    Task {
-      mock.property = "Fish"
-      mock.property = "Kracken"
-      mock.property = "Chips"
-    }
-
-    // then
-    let v1 = try await value
-    XCTAssertEqual(v1, "Kracken")
-  }
-
-  func testOptionalWaiter_WaitsForPredicate() async throws  {
-      let mock = Mock<Int?>(nil)
-      async let value = mock.$property.some(where: { $0 > 5 })
-
-      // when
-      Task {
-        mock.property = 3
-        mock.property = nil
-        mock.property = 10
-      }
-
-      // then
-      let v1 = try await value
-      XCTAssertEqual(v1, 10)
-  }
-
-  func testCollectionWaiter_WaitsForElementAtIndex() async throws  {
-    let mock = Mock(Array<Int>())
-    async let value = mock.$property.value(at: 2)
+    func testInitializesWithValue() async throws {
+        // given
+        let mock = Mock<Int?>(10)
 
         // when
-    Task {
-      mock.property.append(10)
-      mock.property.append(20)
-      mock.property.append(30)
+        let value = try await mock.$property.some()
+
+        // then
+        XCTAssertEqual(value, 10)
     }
 
-    // then
-    let v1 = try await value
-    XCTAssertEqual(v1, 30)
-  }
+    func testLateTask_ReceivesTheWrappedValue() async throws  {
+        // given
+        let mock = Mock<String?>("Fish")
+        mock.property = "Chips"
 
-  func testEquatableWaiter_WaitsForElement() async throws  {
-    let mock = Mock(0)
-    async let value = mock.$property.equals(30)
+        // when
+        let value = try await mock.$property.some()
 
-    // when
-    Task {
-      mock.property = 10
-      mock.property = 20
-      mock.property = 30
+        // then
+        XCTAssertEqual(value, "Chips")
     }
 
-    // then
-    let v1 = try await value
-    XCTAssertEqual(v1, 30)
-  }
+    func testMultipleTasks_ReceiveTheWrappedValue() async throws {
+        // given
+        let mock = Mock<String?>(nil)
 
-  func testEquatableWaiter_WaitsForOptionalElement() async throws  {
-    let mock = Mock(Optional<String>.none)
-    async let value = mock.$property.equals("Fish")
+        async let value1 = mock.$property.some()
+        async let value2 = mock.$property.some()
+        async let value3 = mock.$property.some()
 
-    // when
-    Task {
-      mock.property = "chips"
-      mock.property = "fish"
-      mock.property = "Fish"
+        // when
+        Task { mock.property = "Chips" }
+
+        // then
+        let v1 = try await value1
+        XCTAssertEqual(v1, "Chips")
+
+        let v2 = try await value2
+        XCTAssertEqual(v2, "Chips")
+
+        let v3 = try await value3
+        XCTAssertEqual(v3, "Chips")
     }
 
-    // then
-    let v1 = try await value
-    XCTAssertEqual(v1, "Fish")
-  }
+    func testWaitersAreRemoved_WhenComplete() async throws  {
+        // given
+        let mock = Mock<String?>(nil)
+        async let value1 = mock.$property.some()
 
-  func testModify_TriggersWaiter() async throws {
-    // given
-    let mock = Mock<Int?>(nil)
-    async let value = mock.$property.some()
+        // when
+        mock.property = "Chips"
+        _ = try await value1
 
-    // when
-    mock.modify { $0 = 200 }
+        // then
+        XCTAssertTrue(mock.isWaitingEmpty)
+    }
 
-    // then
-    let v1 = try await value
-    XCTAssertEqual(v1, 200)
-  }
+    func testNil_MakesTaskWait() async throws {
+        // given
+        let mock = Mock<String?>("Fish")
+        mock.property = "Fish"
+        mock.property = nil
+        async let value1 = mock.$property.some()
+
+        // when
+        mock.property = "Chips"
+
+        // then
+        let v1 = try await value1
+        XCTAssertEqual(v1, "Chips")
+    }
+
+    func testCancellingTask_ThrowsCancellationError() async {
+        // given
+        let mock = Mock<String?>(nil)
+        let task = Task<String, Error> {
+            try await mock.$property.some()
+        }
+
+        // when
+        task.cancel()
+
+        // then
+        do {
+            _ = try await task.value
+            XCTFail("Expected Error")
+        } catch {
+            XCTAssertTrue(error is CancellationError)
+        }
+    }
+
+    func testCollectionWaiter_WaitsForMinimumElements() async throws  {
+        let mock = Mock("")
+        async let value = mock.$property.first(withAtLeast: 5)
+
+        // when
+        Task {
+            mock.property = "Fish"
+            mock.property = "Kracken"
+            mock.property = "Chips"
+        }
+
+        // then
+        let v1 = try await value
+        XCTAssertEqual(v1, "Kracken")
+    }
+
+    func testOptionalWaiter_WaitsForPredicate() async throws  {
+        let mock = Mock<Int?>(nil)
+        async let value = mock.$property.some(where: { $0 > 5 })
+
+        // when
+        Task {
+            mock.property = 3
+            mock.property = nil
+            mock.property = 10
+        }
+
+        // then
+        let v1 = try await value
+        XCTAssertEqual(v1, 10)
+    }
+
+    func testCollectionWaiter_WaitsForElementAtIndex() async throws  {
+        let mock = Mock(Array<Int>())
+        async let value = mock.$property.value(at: 2)
+
+        // when
+        Task {
+            mock.property.append(10)
+            mock.property.append(20)
+            mock.property.append(30)
+        }
+
+        // then
+        let v1 = try await value
+        XCTAssertEqual(v1, 30)
+    }
+
+    func testEquatableWaiter_WaitsForElement() async throws  {
+        let mock = Mock(0)
+        async let value = mock.$property.equals(30)
+
+        // when
+        Task {
+            mock.property = 10
+            mock.property = 20
+            mock.property = 30
+        }
+
+        // then
+        let v1 = try await value
+        XCTAssertEqual(v1, 30)
+    }
+
+    func testEquatableWaiter_WaitsForOptionalElement() async throws  {
+        let mock = Mock(Optional<String>.none)
+        async let value = mock.$property.equals("Fish")
+
+        // when
+        Task {
+            mock.property = "chips"
+            mock.property = "fish"
+            mock.property = "Fish"
+        }
+
+        // then
+        let v1 = try await value
+        XCTAssertEqual(v1, "Fish")
+    }
+
+    func testModify_TriggersWaiter() async throws {
+        // given
+        let mock = Mock<Int?>(nil)
+        async let value = mock.$property.some()
+
+        // when
+        mock.modify { $0 = 200 }
+
+        // then
+        let v1 = try await value
+        XCTAssertEqual(v1, 200)
+    }
 }
 
 final class Mock<T>: @unchecked Sendable {
-  @Awaiting var property: T
+    @Awaiting var property: T
 
-  init(_ initial: T) {
-    self.property = initial
-  }
+    init(_ initial: T) {
+        self.property = initial
+    }
 
-  var isWaitingEmpty: Bool {
-    _property.isWaitingEmpty
-  }
+    var isWaitingEmpty: Bool {
+        _property.isWaitingEmpty
+    }
 
-  @discardableResult
-  func modify<U>(_ transform: (inout T) throws -> U) rethrows -> U {
-    try _property.modify(transform)
-  }
+    @discardableResult
+    func modify<U>(_ transform: (inout T) throws -> U) rethrows -> U {
+        try _property.modify(transform)
+    }
 }
